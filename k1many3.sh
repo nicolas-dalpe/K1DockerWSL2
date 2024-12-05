b3a21f02ed10bba21ba734cd73fb8e12d7255434
nicolas-dalpe <nicolas.dalpe@knowledgeone.ca>
2024-12-05 10:11:29 -0500

    MECONTRACK-245 - Added k1many3.sh to add xDebug with --build and --start.

--- k1many3.sh
+++ k1many3.sh
@@ -30,7 +30,7 @@ info_message() {
     CYAN='\033[0;36m'
     NC='\033[0m' # No Color
     echo
-    echo "${CYAN}$1${NC}"
+    echo -e "${CYAN}$1${NC}"
     echo
 }
 
@@ -40,7 +40,7 @@ error_message() {
     RED='\033[0;31m'
     NC='\033[0m' # No Color
     echo
-    echo "${RED}$1${NC}"
+    echo -e "${RED}$1${NC}"
     echo
 }
 
@@ -51,6 +51,25 @@ start_server() {
     #  sleep 6
     # Just in case there is still some latency.
     bin/moodle-docker-wait-for-db
+}
+
+start_instances() {
+    # xDebug port. Must be different for each instances.
+    xdebugport=$1
+
+    # Reset the xDebug config file from previous boot.
+    # We reset it to set the client port back 9003 to
+    # make sure the sed will find and replace it with
+    # the new client port.
+    git checkout assets/php/docker-php-ext-xdebug.ini
+
+    # Change xDebug port for this instance.
+    sed -i -e 's/xdebug.client_port=9003/xdebug.client_port='"${xdebugport}"'/g' assets/php/docker-php-ext-xdebug.ini
+
+    # Starts the container and wait for DB.
+    start_server
+
+    info_message "${folder} site started at http://localhost:${MOODLE_DOCKER_WEB_PORT}"
 }
 
 # Function to group the commands to build the each instances.
@@ -114,13 +133,16 @@ exists_in_list() {
     return 0
 }
 
-# Variables
-# MOODLE_DOCKER_DB          - database used by Moodle - default maria db
-# MOODLE_DOCKER_WWWROOT     - folder where the Moodle code is located;
-# MOODLE_DOCKER_PORT        - port (default 8000);
-# MOODLE_DOCKER_PHP_VERSION - php version used in Moodle - default 8.1;
-# COMPOSE_PROJECT_NAME      - Docker project name - used to identify sites;
+validate_folder_path() {
+    if [ ! -d "${1}" ]; then
+        error_message "${1} is not valid"
+        help_messages
+        exit 1
+    fi
+}
 
+##################
+### Validation ###
 if [ $# -eq 0 ];  then
     error_message "No arguments supplied"
     help_messages
@@ -133,132 +155,129 @@ if [ $# -lt 1 ] || [ $# -gt 4 ] ;  then
     exit 1
 fi
 
-## Validate --switch
-## Validate folders
-## Export global vars
-## Call the switch func
-
 list_of_options="--build --destroy --help --reboot --stop --start --restart"
+SWITCH=$1
+if  exists_in_list "$list_of_options" " " $SWITCH;  then
+    error_message "Invalid option $SWITCH."
+    help_messages
+    exit 1
+fi
+
+# Display help message if the --help switch is present.
+if [ "$SWITCH" = "--help" ]
+then
+    help_messages
+    exit 0
+fi
+
+########################
+### Global variables ###
+# MOODLE_DOCKER_DB          - database used by Moodle - default maria db
+# MOODLE_DOCKER_WWWROOT     - folder where the Moodle code is located;
+# MOODLE_DOCKER_PORT        - port (default 8000);
+# MOODLE_DOCKER_PHP_VERSION - php version used in Moodle - default 8.1;
+# COMPOSE_PROJECT_NAME      - Docker project name - used to identify sites;
+
+# We always use Maria DB for all our Moodle.
+export MOODLE_DOCKER_DB=mariadb
+
+# Current working dir.
 cwd=$( dirname "$PWD" )
-count=0
-for var in "$@"
+
+# Moodle Docker Port.
+# AKA which port the webserver container will listen to.
+moodleDockerPort=(8000 1234 5678)
+
+# xDebug ports
+# Each containers must have a their own port.
+xdebugPort=(9003 9004 9005)
+
+# Use the multiple instances local.yml file.
+cp local.yml_many local.yml
+
+################################################
+### Process swithes that don't need options. ###
+case $SWITCH in
+    "--destroy")
+        if ! docker ps | grep -q 'moodlehq'; then
+            info_message "No containers running. Nothing to shutdown."
+            exit 1
+        fi
+        docker stop $(docker ps -a -q)
+        docker rm $(docker ps -a -q)
+        reset_config_files
+        info_message "All containers shut down and removed."
+        exit 1
+        ;;
+    "--restart")
+        if ! docker ps | grep -q 'moodlehq'; then
+            info_message "No containers running. Nothing to reboot."
+            exit 1
+        fi
+        docker restart $(docker ps -q)
+        info_message "All sites restarted."
+        exit 1
+        ;;
+    "--stop")
+        if ! docker ps | grep -q 'moodlehq'; then
+            info_message "No containers running. Nothing to stop."
+            exit 1
+        fi
+        docker stop $(docker ps -q)
+        info_message "All sites stopped."
+        exit 1
+        ;;
+esac
+
+#############################################
+### Process swithes that require options. ###
+
+# Ignore first parm passed to the script.
+# Skip the --switch to cycle the folders in argument.
+shift
+
+# Iterate the passed folders.
+iteratior=0
+for i in "$@"
 do
-    count=$((count+1))
-    if [ "$count" -eq 1 ]; then
-        SWITCH=${var}
-        # Check to see if the list of arguments is valid.
-        if  exists_in_list "$list_of_options" " " $SWITCH;  then
-            error_message "Invalid option $SWITCH."
-            help_messages
-            exit 1
-        fi
-        # Check for any swithes that don't need options.
-        case $SWITCH in
-           "--build")
-                if [ -n "$(docker ps -f "name=${var}-webserver-1" -f "status=running" -q )" ]; then
-                    echo "The first site is already running!. It cannot be re-initialized."
-                    exit 1
-                fi
-                # Do the basics to start the site.
-                export MOODLE_DOCKER_DB=mariadb
-                export COMPOSE_PROJECT_NAME=site1
-                cp local.yml_many local.yml
-                ;;
-           "--help")
-              help_messages
-              exit 1
-              ;;
-            "--destroy")
-                if ! docker ps | grep -q 'moodlehq'; then
-                   info_message "No containers running. Nothing to shutdown."
-                   exit 1
-                fi
-                docker stop $(docker ps -a -q)
-                docker rm $(docker ps -a -q)
-                reset_config_files
-                info_message "All containers shut down and removed."
-                exit 1
-                ;;
-            "--restart")
-                if ! docker ps | grep -q 'moodlehq'; then
-                     info_message "No containers running. Nothing to reboot."
-                     exit 1
-                fi
-                docker restart $(docker ps -q)
-                info_message "All sites restarted."
-                exit 1
-                ;;
-            "--start")
-                if [ -n "$(docker ps -f "name=site1-webserver-1" -f "status=running" -q )" ]; then
-                     info_message "Sites are already running."
-                     exit 1
-                fi
-                docker start $(docker ps -a -q -f status=exited)
-                info_message "All sites started."
-                exit 1
-                ;;
-            "--stop")
-                if ! docker ps | grep -q 'moodlehq'; then
-                   info_message "No containers running. Nothing to stop."
-                   exit 1
-                fi
-                docker stop $(docker ps -q)
-                info_message "All sites stopped."
-                exit 1
-                ;;
-        esac
-    else
-        # Used to name the project according to the folder name
-        # which makes it easier to recognize who is who in Docker.
-        projectname="${var}"
+    # Folder passed in args to the script.
+    argfolder="$i"
 
-        # Full path of the folder containing the Moodle files.
-        folder="${cwd}/${var}"
-        if [ ! -d "${folder}" ]; then
-            echo "${folder} is not valid"
-            exit 1
-        fi
+    # Full system path of the Moodle instance.
+    validate_folder_path "${cwd}/${argfolder}"
+    fullfolderpath="${cwd}/${argfolder}"
+    export MOODLE_DOCKER_WWWROOT=${fullfolderpath}
 
-        # Start the site
-        # Check the Moodle version. If its 4.5 then set php version to 8.3
-        export MOODLE_DOCKER_PHP_VERSION=8.1
-        moodlever=$(grep "$branch   = '405';" $folder/version.php)
-        if [ "$moodlever" ]; then
-           export MOODLE_DOCKER_PHP_VERSION=8.3
-        fi
-        export MOODLE_DOCKER_WWWROOT=${folder}
-        export COMPOSE_PROJECT_NAME=${projectname}
-        cp config.docker-template.php $MOODLE_DOCKER_WWWROOT/config.php
-        case $count in
-           "2")
-               export MOODLE_DOCKER_WEB_PORT=8000
-               # Starts the container and wait for DB.
-            #    start_server
-               # Install Moodle.
-            #    bin/moodle-docker-compose exec webserver php admin/cli/install_database.php --agree-license --fullname="${projectname}" --shortname="${projectname}" --summary="${projectname}" --adminpass="test" --adminemail="admin@example.com"
-               # Set the session cookie to avoid login problem between instances.
-            #    bin/moodle-docker-compose exec webserver php admin/cli/cfg.php --name=sessioncookie --set="${projectname}"
-            #    info_message "${folder} site started - http://localhost:${MOODLE_DOCKER_WEB_PORT}"
-               build_instances ${projectname} ${folder} 9003
-            ;;
-            "3")
-               export MOODLE_DOCKER_WEB_PORT=1234
-            #    start_server
-            #    bin/moodle-docker-compose exec webserver php admin/cli/install_database.php --agree-license --fullname="${projectname}" --shortname="${projectname}" --summary="${projectname}" --adminpass="test" --adminemail="admin@example.com"
-            #    bin/moodle-docker-compose exec webserver php admin/cli/cfg.php --name=sessioncookie --set="${projectname}"
-            #    info_message "${folder} site started - http://localhost:${MOODLE_DOCKER_WEB_PORT}"
-               build_instances ${projectname} ${folder} 9004
-            ;;
-            "4")
-               export MOODLE_DOCKER_WEB_PORT=6789
-            #    start_server
-            #    bin/moodle-docker-compose exec webserver php admin/cli/install_database.php --agree-license --fullname="${projectname}" --shortname="${projectname}" --summary="${projectname}" --adminpass="test" --adminemail="admin@example.com"
-            #    bin/moodle-docker-compose exec webserver php admin/cli/cfg.php --name=sessioncookie --set="${projectname}"
-            #    info_message "${folder} site started - http://localhost:${MOODLE_DOCKER_WEB_PORT}"
-               build_instances ${projectname} ${folder} 9005
-            ;;
-         esac
+    # Check the Moodle version. If its 4.5 then set php version to 8.3.
+    export MOODLE_DOCKER_PHP_VERSION=8.1
+    moodlever=$(grep "$branch   = '405';" $fullfolderpath/version.php)
+    if [ "$moodlever" ]; then
+        export MOODLE_DOCKER_PHP_VERSION=8.3
     fi
-done
 
-return
+    # Used to name the project according to the folder name
+    # which makes it easier to recognize who is who in Docker.
+    projectname="${argfolder}"
+    export COMPOSE_PROJECT_NAME=${projectname}
+
+    case $SWITCH in
+        "--build")
+            if [ -n "$(docker ps -f "name=${projectname}-webserver-1" -f "status=running" -q )" ]; then
+                error_message "The first site is already running! It cannot be re-initialized."
+                exit 1
+            fi
+            export MOODLE_DOCKER_WEB_PORT="${moodleDockerPort[$iteratior]}"
+            cp config.docker-template.php $MOODLE_DOCKER_WWWROOT/config.php
+            build_instances ${projectname} ${fullfolderpath} "${xdebugPort[$iteratior]}"
+            ;;
+        "--start")
+            export MOODLE_DOCKER_WEB_PORT="${moodleDockerPort[$iteratior]}"
+            cp config.docker-template.php $MOODLE_DOCKER_WWWROOT/config.php
+            start_instances "${xdebugPort[$iteratior]}"
+            ;;
+    esac
+
+    # Increment loop counter.
+    iteratior=$((iteratior+1))
+done
+exit 0
